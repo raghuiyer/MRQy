f06115f026abf3be0dc1e8f1bf1b257d825099a7
Raghavendra Iyer <raghavendraiyer@Raghavendras-MacBook-Pro.local>
2020-08-08 17:18:46 -0400

    updated README

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name="mrqy",
-package_dir={'': 'src'}, packages=find_packages(where='src'))
+setup(
+    name="mrqy",
+    version="0.1",
+    package_dir={'': 'src'}, 
+    packages=find_packages(where='src'))
